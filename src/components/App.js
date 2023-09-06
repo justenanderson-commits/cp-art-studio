dd83c5746df46fb5bc2b24847aeecb81bf528549
Justen Anderson <justenanderson@gmail.com>
2023-09-06 08:58:32 -0600

    Set up routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,27 @@
-
-import '../App.css';
+import '../App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './Home'
+import About from './About'
+import Testimonials from './Testimonials'
+import Gallery from './Gallery'
+import Contact from './Contact'
 
 function App() {
   return (
     <div className="App">
-     App component
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/testimonials" element={<Testimonials />} />
+          <Route path="/gallery" element={<Gallery />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Router>
+      App component
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
