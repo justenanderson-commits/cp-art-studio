fb2ef9cc1c2dff3553521d5c595c28e6479c6a21
Justen Anderson <justenanderson@gmail.com>
2023-09-06 09:04:27 -0600

    Add components

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import '../App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Header from './Header'
 import Home from './Home'
 import About from './About'
 import Testimonials from './Testimonials'
@@ -19,7 +20,6 @@ function App() {
           <Route path="/contact" element={<Contact />} />
         </Routes>
       </Router>
-      App component
     </div>
   )
 }
