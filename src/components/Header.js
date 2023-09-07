fea7ebae1e61476ba105b22d6f02417cc7318f30
Justen Anderson <justenanderson@gmail.com>
2023-09-07 13:41:35 -0600

    Make header screen-width and adjust link padding

--- src/components/Header.js
+++ src/components/Header.js
@@ -22,43 +22,43 @@ const Header = () => {
 
   const navList = (
     <ul className="mb-4 mt-2 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:justify-evenly lg:gap-10">
-      <Typography
-        as="li"
-        className="p-1 font-normal"
-      >
-        <a href="/" className="flex items-center text-sage hover:text-white active:text-white">
+      <Typography as="li" className="p-1 px-10 font-normal">
+        <a
+          href="/"
+          className="flex items-center text-sage hover:text-white active:text-white"
+        >
           Home
         </a>
       </Typography>
-      <Typography
-        as="li"
-       className="p-1 font-normal"
-      >
-        <a href="/about" className="flex items-center text-sage hover:text-white">
+     <Typography as="li" className="p-1 px-10 font-normal">
+        <a
+          href="/about"
+          className="flex items-center text-sage hover:text-white"
+        >
           About
         </a>
       </Typography>
-      <Typography
-        as="li"
-       className="p-1 font-normal"
-      >
-        <a href="/testimonials" className="flex items-center text-sage hover:text-white">
+     <Typography as="li" className="p-1 px-10 font-normal">
+        <a
+          href="/testimonials"
+          className="flex items-center text-sage hover:text-white"
+        >
           Testimonials
         </a>
       </Typography>
-      <Typography
-        as="li"
-       className="p-1 font-normal"
-      >
-        <a href="/gallery" className="flex items-center text-sage hover:text-white">
+     <Typography as="li" className="p-1 px-10 font-normal">
+        <a
+          href="/gallery"
+          className="flex items-center text-sage hover:text-white"
+        >
           Gallery
         </a>
       </Typography>
-      <Typography
-        as="li"
-       className="p-1 font-normal"
-      >
-        <a href="/contact" className="flex items-center text-sage hover:text-white">
+     <Typography as="li" className="p-1 px-10 font-normal">
+        <a
+          href="/contact"
+          className="flex items-center text-sage hover:text-white"
+        >
           Contact
         </a>
       </Typography>
@@ -66,61 +66,54 @@ const Header = () => {
   )
 
   return (
-      <Navbar className="w-screen mx-auto py-2 px-4 lg:px-2 lg:py-4 bg-black rounded-none">
 
-
-
-        <div className="container mx-auto flex items-center justify-evenly text-blue-gray-900">
-
-
-
-          <div className="hidden flex w-full items-center lg:block">{navList}</div>
-          <IconButton
-            variant="text"
-            className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
-            ripple={false}
-            onClick={() => setOpen(!open)}
-          >
-            {open ? (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                className="h-6 w-6"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                strokeWidth={2}
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M6 18L18 6M6 6l12 12"
-                />
-              </svg>
-            ) : (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth={2}
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M4 6h16M4 12h16M4 18h16"
-                />
-              </svg>
-            )}
-          </IconButton>
-        </div>
-        <Collapse open={open}>
+    <div className="p-2 bg-black">
+      <div className="flex items-center justify-evenly">
+        <div className="hidden flex items-center lg:block">{navList}</div>
+        <IconButton
+          variant="text"
+          className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
+          ripple={false}
+          onClick={() => setOpen(!open)}
+        >
+          {open ? (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              className="h-6 w-6"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth={2}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+          ) : (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth={2}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M4 6h16M4 12h16M4 18h16"
+              />
+            </svg>
+          )}
+        </IconButton>
+      </div>
+      <Collapse open={open}>
         <Card className="my-4 mx-auto w-9/12">
-          <CardBody className="mx-auto">
-            {navList}
-          </CardBody>
+          <CardBody className="mx-auto">{navList}</CardBody>
         </Card>
       </Collapse>
-      </Navbar>
+    </div>
   )
 }
 
