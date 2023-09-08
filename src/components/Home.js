3264d4d2085a88c765fddd6c8c72911ff2110683
Justen Anderson <justenanderson@gmail.com>
2023-09-08 10:05:21 -0600

    Remove unused code

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,21 +7,29 @@ import { Button, Card, Typography } from '@material-tailwind/react'
 const Home = () => {
   return (
     <div className="relative home-page text-white text-2xl min-h-screen bg-black pt-8 lg:pt-20 flex flex-col items-center">
-      <Typography className='mr-auto px-4'>
-        Students progress through the courses on an individual basis. Enrollment
-        for new students is always open!
-      </Typography>
-      <div id='cp-title-container' className="bg-white w-full flex justify-center items-center" style={{ width: '100%', height: '200px' }}>
+      <div className="mr-auto -mt-8 mb-2 w-5/12">
+        <Typography className="mr-auto px-4">
+          Students progress through the courses on an individual basis.
+          Enrollment for new students is always open!
+        </Typography>
+      </div>
+      <div
+        id="cp-title-container"
+        className="flex justify-center items-center"
+        style={{ width: '100%', height: '200px' }}
+      >
         <Typography
-          id='cp-title-title' 
           variant="h1"
           className="pr-24 font-serif font-bold text-12xl"
           style={{ WebkitTextStroke: '2px teal' }}
         >
           CP Art
         </Typography>
-          </div>
-        <div className='bg-yellow-300 flex items-center justify-center' style={{ width: '100%', height: '200px' }}>
+      </div>
+      <div
+        className="flex items-center justify-center"
+        style={{ width: '100%', height: '200px' }}
+      >
         <Typography
           as="span"
           variant="h1"
@@ -30,7 +38,7 @@ const Home = () => {
         >
           Studio
         </Typography>
-        </div>
+      </div>
 
       <About />
       <Testimonials />
