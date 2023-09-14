a874e1ffd2ad77b55699d1cfbe33062b0f8c7dc1
Justen Anderson <justenanderson@gmail.com>
2023-09-14 16:39:51 -0600

    Add images to About page.

--- src/components/About.js
+++ src/components/About.js
@@ -1,4 +1,7 @@
-import React from 'react'
+import connieAndSherman from '../assets/c_and_sherm 1.png'
+import connieAndDaughter from '../assets/c_and_m 1.png'
+import canyon from '../assets/m_sherm_canyon 1.png'
+
 const About = () => {
   return (
     <div className="bg-black flex flex-row justify-center w-full">
@@ -23,20 +26,18 @@ const About = () => {
         <img
           className="absolute w-[1140px] h-[490px] top-[225px] left-[186px] object-cover"
           alt="M sherm canyon"
-          src="m-sherm-canyon-1.png"
+          src={canyon}
         />
         <img
           className="absolute w-[518px] h-[330px] top-[1291px] left-[186px] object-cover"
           alt="C and m"
-          src="c-and-m-1.png"
+          src={connieAndDaughter}
         />
         <img
           className="absolute w-[518px] h-[330px] top-[1291px] left-[808px] object-cover"
           alt="C and sherm"
-          src="c-and-sherm-1.png"
+          src={connieAndSherman}
         />
-
-
 
         {/* <div className="absolute w-[1506px] h-[72px] top-0 left-[-5px] bg-[#1e1d1d]">
           <div className="left-[385px] absolute top-[22px] [font-family:'Inter-Medium',Helvetica] font-medium text-[#a0bfb5] text-[22px] tracking-[0] leading-[normal]">
@@ -62,4 +63,4 @@ const About = () => {
   )
 }
 
-export default About  
+export default About
