d86210ec4d430ef85485fcfda269da93bb9a203d
Justen Anderson <justenanderson@gmail.com>
2023-09-15 17:25:24 -0600

    Adjust margin of top image

--- src/components/About.js
+++ src/components/About.js
@@ -8,7 +8,7 @@ const About = () => {
       <div className="bg-black overflow-hidden relative">
         <div className="[font-family:'Playfair_Display-Regular',Helvetica] font-normal text-white text-5xl lg:text-6xl md:text-4xl text-center my-8">
           About
-          <div className="flex justify-center">
+          <div className="flex justify-center lg:mt-4">
             <img
               className="w-full lg:w-3/4 lg:h-[490px] md:w-5/6 md:h-[424px] object-cover mt-4"
               alt="M sherm canyon"
